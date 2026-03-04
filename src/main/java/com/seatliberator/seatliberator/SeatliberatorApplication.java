13701a241b3f17186ad38e9e8b6a49cec968239d
lilamaris <lilamaris.dev@gmail.com>
2026-03-04 10:44:34 +0900

    initial

---
+++ src/main/java/com/seatliberator/seatliberator/SeatliberatorApplication.java
@@ -0,0 +1,13 @@
+package com.seatliberator.seatliberator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatliberatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatliberatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seatliberator/seatliberator/SeatliberatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seatliberator.seatliberator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatliberatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
