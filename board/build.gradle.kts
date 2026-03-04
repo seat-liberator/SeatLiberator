3cf93a20059a055793ab4647deec0ad16f4b702c
lilamaris <lilamaris.dev@gmail.com>
2026-03-04 16:52:13 +0900

    feat(board): 프로젝트 초기 설정

---
+++ board/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+}
+
+group = "com.seatliberator.seatliberator"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Spring Web MVC
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+
+    // Spring Data Jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // Lombok
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("org.postgresql:postgresql")
+    annotationProcessor("org.projectlombok:lombok")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
