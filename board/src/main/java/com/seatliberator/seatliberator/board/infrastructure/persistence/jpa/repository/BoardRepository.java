fbe7b9b47bd62cc68fb88749136332c79a2ca8f2
lilamaris <lilamaris.dev@gmail.com>
2026-03-05 09:26:11 +0900

    fix(board): 불필요한 import 제거
    - BoardStore 인터페이스의 Jpa 구현체(J paBoardStore)가 있는데, BoardRepository 또한 인터페이스를 참조하고 있었음

--- board/src/main/java/com/seatliberator/seatliberator/board/infrastructure/persistence/jpa/repository/BoardRepository.java
+++ board/src/main/java/com/seatliberator/seatliberator/board/infrastructure/persistence/jpa/repository/BoardRepository.java
@@ -1,10 +1,9 @@
 package com.seatliberator.seatliberator.board.infrastructure.persistence.jpa.repository;
 
-import com.seatliberator.seatliberator.board.application.port.out.BoardStore;
 import com.seatliberator.seatliberator.board.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface BoardRepository extends JpaRepository<Board, UUID>, BoardStore {
+public interface BoardRepository extends JpaRepository<Board, UUID> {
 }
