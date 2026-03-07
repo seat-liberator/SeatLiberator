273d5754f78d3befdb9fafe83673c437b011ae89
lilamaris <lilamaris.dev@gmail.com>
2026-03-07 23:47:20 +0900

    fix(identity.core): null checking 추가
    - IntrospectionFactory의 createIntrospection 메서드 파라미터에 @NonNull 계약 추가
    - IntrospectionFactory 기본 구현체 SimpleIntrospectionFactory에서 런타임 Null 방어

--- identity/identity-core/src/main/java/com/seatliberator/seatliberator/identity/core/introspect/IntrospectionFactory.java
+++ identity/identity-core/src/main/java/com/seatliberator/seatliberator/identity/core/introspect/IntrospectionFactory.java
@@ -1,9 +1,10 @@
 package com.seatliberator.seatliberator.identity.core.introspect;
 
 import com.seatliberator.seatliberator.identity.core.actor.Actor;
+import org.jspecify.annotations.NonNull;
 
 public interface IntrospectionFactory {
     Introspection createNoContent();
 
-    Introspection createIntrospection(Long expiration, Actor actor);
+    Introspection createIntrospection(@NonNull Long expiration, @NonNull Actor actor);
 }
--- identity/identity-core/src/main/java/com/seatliberator/seatliberator/identity/core/introspect/SimpleIntrospectionFactory.java
+++ identity/identity-core/src/main/java/com/seatliberator/seatliberator/identity/core/introspect/SimpleIntrospectionFactory.java
@@ -1,6 +1,9 @@
 package com.seatliberator.seatliberator.identity.core.introspect;
 
 import com.seatliberator.seatliberator.identity.core.actor.Actor;
+import org.jspecify.annotations.NonNull;
+
+import java.util.Objects;
 
 public class SimpleIntrospectionFactory implements IntrospectionFactory {
 
@@ -10,7 +13,9 @@ public class SimpleIntrospectionFactory implements IntrospectionFactory {
     }
 
     @Override
-    public Introspection createIntrospection(Long expiration, Actor actor) {
+    public Introspection createIntrospection(@NonNull Long expiration, @NonNull Actor actor) {
+        Objects.requireNonNull(expiration);
+        Objects.requireNonNull(actor);
         return new SimpleIntrospection(true, expiration, actor);
     }
 }
