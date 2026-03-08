420a96274ed178642836781701a6900168a8241a
lilamaris <lilamaris.dev@gmail.com>
2026-03-08 11:26:03 +0900

    fix(identity.core): expirationMs 기본 값 지정
    - @ConfigurationProperties annotated 된 프로퍼티 클래스 `IntrospectionConfigurationProperties`의 필드 `expirationMs` 기본 값 @DefaultValue 어노테이션으로 추가함

--- identity/identity-core/src/main/java/com/seatliberator/seatliberator/identity/core/introspect/IntrospectionConfigurationProperties.java
+++ identity/identity-core/src/main/java/com/seatliberator/seatliberator/identity/core/introspect/IntrospectionConfigurationProperties.java
@@ -1,9 +1,10 @@
 package com.seatliberator.seatliberator.identity.core.introspect;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 
 @ConfigurationProperties(prefix = "identity.introspection")
 public record IntrospectionConfigurationProperties(
-        Long expirationMs
+        @DefaultValue("900000") Long expirationMs
 ) {
 }
