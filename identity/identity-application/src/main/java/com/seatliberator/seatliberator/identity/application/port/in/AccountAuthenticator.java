61c3ef1b716bfd224e949013278d9cba799db41b
lilamaris <lilamaris.dev@gmail.com>
2026-03-10 19:29:05 +0900

    fix(identity.application): AccountAuthenticator 스펙 변경
    - Actor는 identity.core 모듈 스펙인데 application 레이어에서 사용하는게 적절하지 않음
    - Authenticator에서 더 풍부한 인증 정보가 필요해서 AuthContextEntry를 작성하고 이를 반환하는 메서드 스펙으로 변경함

--- identity/identity-application/src/main/java/com/seatliberator/seatliberator/identity/application/port/in/AccountAuthenticator.java
+++ identity/identity-application/src/main/java/com/seatliberator/seatliberator/identity/application/port/in/AccountAuthenticator.java
@@ -1,10 +1,10 @@
 package com.seatliberator.seatliberator.identity.application.port.in;
 
 import com.seatliberator.seatliberator.identity.application.port.in.command.AuthenticationCommand;
-import com.seatliberator.seatliberator.identity.core.actor.Actor;
+import com.seatliberator.seatliberator.identity.application.port.in.result.AuthContextEntry;
 
 public interface AccountAuthenticator {
-    Actor authenticate(AuthenticationCommand.Credential command);
+    AuthContextEntry.Credential authenticate(AuthenticationCommand.Credential command);
 
-    Actor authenticate(AuthenticationCommand.Federated command);
+    AuthContextEntry.Federated authenticate(AuthenticationCommand.Federated command);
 }
