2600514e9bc9f0dccb47cee6a4ec085753a76aaa
lilamaris <lilamaris.dev@gmail.com>
2026-03-10 19:40:08 +0900

    feat(identity.application): port.in 스펙 구현체 추가

---
+++ identity/identity-application/src/main/java/com/seatliberator/seatliberator/identity/application/service/AccountAuthenticationService.java
@@ -0,0 +1,47 @@
+package com.seatliberator.seatliberator.identity.application.service;
+
+import com.seatliberator.seatliberator.identity.application.exception.ApplicationErrorCode;
+import com.seatliberator.seatliberator.identity.application.exception.ApplicationException;
+import com.seatliberator.seatliberator.identity.application.port.in.AccountAuthenticator;
+import com.seatliberator.seatliberator.identity.application.port.in.command.AuthenticationCommand;
+import com.seatliberator.seatliberator.identity.application.port.in.result.AuthContextEntry;
+import com.seatliberator.seatliberator.identity.application.port.out.CredentialAccountStore;
+import com.seatliberator.seatliberator.identity.application.port.out.FederatedAccountStore;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AccountAuthenticationService implements AccountAuthenticator {
+    private final CredentialAccountStore credentialAccountStore;
+    private final FederatedAccountStore federatedAccountStore;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public AuthContextEntry.Credential authenticate(AuthenticationCommand.Credential command) {
+        return credentialAccountStore.findCredentialAuthByEmail(command.email())
+                .filter(auth -> passwordEncoder.matches(command.password(), auth.passwordHash()))
+                .map(auth -> new AuthContextEntry.Credential(
+                        auth.userId(),
+                        auth.email(),
+                        auth.passwordHash()
+                ))
+                .orElseThrow(() -> new ApplicationException(ApplicationErrorCode.AUTHENTICATION_FAILED));
+    }
+
+    @Override
+    public AuthContextEntry.Federated authenticate(AuthenticationCommand.Federated command) {
+        return federatedAccountStore.findFederatedAuthByRegistrationIdAndProviderUserId(
+                        command.registrationId(),
+                        command.providerUserId()
+                )
+                .map(auth -> new AuthContextEntry.Federated(
+                        auth.userId(),
+                        auth.registrationId(),
+                        auth.providerUserId()
+                ))
+                .orElseThrow(() -> new ApplicationException(ApplicationErrorCode.AUTHENTICATION_FAILED));
+    }
+}
---
+++ identity/identity-application/src/main/java/com/seatliberator/seatliberator/identity/application/service/UserRegistrarService.java
@@ -0,0 +1,74 @@
+package com.seatliberator.seatliberator.identity.application.service;
+
+import com.seatliberator.seatliberator.identity.application.exception.ApplicationErrorCode;
+import com.seatliberator.seatliberator.identity.application.exception.ApplicationException;
+import com.seatliberator.seatliberator.identity.application.port.in.UserRegistrar;
+import com.seatliberator.seatliberator.identity.application.port.in.command.RegistrationCommand;
+import com.seatliberator.seatliberator.identity.application.port.in.result.UserEntry;
+import com.seatliberator.seatliberator.identity.application.port.out.CredentialAccountStore;
+import com.seatliberator.seatliberator.identity.application.port.out.FederatedAccountStore;
+import com.seatliberator.seatliberator.identity.application.port.out.UserStore;
+import com.seatliberator.seatliberator.identity.domain.CredentialAccount;
+import com.seatliberator.seatliberator.identity.domain.FederatedAccount;
+import com.seatliberator.seatliberator.identity.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserRegistrarService implements UserRegistrar {
+    private final CredentialAccountStore credentialAccountStore;
+    private final FederatedAccountStore federatedAccountStore;
+    private final UserStore userStore;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserEntry register(RegistrationCommand.Credential command) {
+        boolean existsAccount = credentialAccountStore.existsByEmail(command.email());
+
+        if (existsAccount) throw new ApplicationException(ApplicationErrorCode.EMAIL_DUPLICATED);
+
+        var passwordHash = passwordEncoder.encode(command.password());
+        var user = User.create(command.nickname());
+        var account = CredentialAccount.create(
+                command.email(),
+                passwordHash
+        );
+
+        user.setCredentialAccount(account);
+
+        var savedUser = userStore.save(user);
+
+        return new UserEntry(
+                savedUser.getId(),
+                savedUser.getNickname()
+        );
+    }
+
+    @Override
+    public UserEntry register(RegistrationCommand.Federated command) {
+        boolean existsAccount = federatedAccountStore.existsByRegistrationIdAndProviderUserId(
+                command.registrationId(),
+                command.providerUserId()
+        );
+
+        if (existsAccount) throw new ApplicationException(ApplicationErrorCode.AUTHENTICATION_FAILED);
+
+        var user = User.create(command.nickname());
+        var account = FederatedAccount.create(
+                command.registrationId(),
+                command.providerUserId()
+        );
+
+        user.addFederatedAccount(account);
+
+        var savedUser = userStore.save(user);
+
+        return new UserEntry(
+                savedUser.getId(),
+                savedUser.getNickname()
+        );
+    }
+}
