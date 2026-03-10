746febc57e7c56f05b520050bdb7934f56164132
lilamaris <lilamaris.dev@gmail.com>
2026-03-10 20:21:56 +0900

    feat(identity.application): Credential 인증 흐름 진입점 속성 정의

---
+++ identity/identity-application/src/main/java/com/seatliberator/seatliberator/identity/infrastructure/security/authentication/method/credential/CredentialAuthenticationConfigurationProperties.java
@@ -0,0 +1,18 @@
+package com.seatliberator.seatliberator.identity.infrastructure.security.authentication.method.credential;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.Optional;
+
+@ConfigurationProperties(prefix = "identity.application.auth.credential")
+public record CredentialAuthenticationConfigurationProperties(
+        Endpoint signIn,
+        Endpoint signUp
+) {
+    public CredentialAuthenticationConfigurationProperties {
+        signIn = Optional.ofNullable(signIn)
+                .orElse(new Endpoint("POST", "/sign-in"));
+        signUp = Optional.ofNullable(signUp)
+                .orElse(new Endpoint("POST", "/sign-up"));
+    }
+}
