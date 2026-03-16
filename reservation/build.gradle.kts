c273116b9aa45356ec11b0ab0964e26ad1fee217
lilamaris <lilamaris.dev@gmail.com>
2026-03-16 17:50:51 +0900

    test(reservation): 예약(Reservation) 생성 동시성 테스트 및 좌석(Seat) 생성 테스트 추가
    - 비관락 기반 예약 생성 동시성 방어 로직 테스트 코드 작성함.
      - 동시에 하나의 좌석에 여러 스레드가 같은 시간에 예약을 걸어도 하나의 스레드만 예약이 성공함을 보장함
    - 좌석(Seat) 및 예약(Reservation) 단순 생성 기능 테스트 작성함.

--- reservation/build.gradle.kts
+++ reservation/build.gradle.kts
@@ -24,6 +24,8 @@ dependencies {
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.testcontainers:testcontainers-junit-jupiter")
+    testImplementation("org.testcontainers:postgresql:1.21.3")
+    testImplementation("org.testcontainers:jdbc")
 
     // PostgreSQL
     runtimeOnly("org.postgresql:postgresql")
---
+++ reservation/src/test/java/com/seatliberator/seatliberator/reservation/ReservationTest.java
@@ -0,0 +1,126 @@
+package com.seatliberator.seatliberator.reservation;
+
+import com.seatliberator.seatliberator.reservation.application.port.in.command.ReservationCreateCommand;
+import com.seatliberator.seatliberator.reservation.application.port.in.command.SeatCreateCommand;
+import com.seatliberator.seatliberator.reservation.application.port.out.ReservationStore;
+import com.seatliberator.seatliberator.reservation.application.port.out.SeatStore;
+import com.seatliberator.seatliberator.reservation.application.service.ReservationService;
+import com.seatliberator.seatliberator.reservation.application.service.SeatService;
+import com.seatliberator.seatliberator.reservation.domain.Reservation;
+import com.seatliberator.seatliberator.reservation.domain.Seat;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.Instant;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Testcontainers
+public class ReservationTest {
+
+    private static final PostgreSQLContainer<?> postgres;
+
+    static {
+        postgres = new PostgreSQLContainer<>("postgres:18-alpine")
+                .withDatabaseName("reservation_test")
+                .withUsername("test_user")
+                .withPassword("test_password");
+        postgres.start();
+    }
+
+    @Autowired
+    SeatService seatService;
+    @Autowired
+    ReservationService reservationService;
+    @Autowired
+    SeatStore seatStore;
+    @Autowired
+    ReservationStore reservationStore;
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    @DisplayName("Seat 생성 시 true 반환한다.")
+    void Seat_생성_시_true_반환한다() {
+        var givenRoomId = "room-1";
+        var givenSeatId = "seat-1";
+
+        var seatCreateCommand = new SeatCreateCommand(
+                givenRoomId,
+                givenSeatId
+        );
+
+        // Then
+        boolean result = seatService.create(seatCreateCommand);
+
+        Optional<Seat> optStoreSeat = seatStore.findByRoomIdAndSeatId(
+                givenRoomId,
+                givenSeatId
+        );
+
+        assertThat(optStoreSeat.isPresent()).isTrue();
+
+        var storedSeat = optStoreSeat.get();
+
+        assertThat(result).isTrue();
+        assertThat(storedSeat.getRoomId()).isEqualTo(givenRoomId);
+        assertThat(storedSeat.getSeatId()).isEqualTo(givenSeatId);
+    }
+
+    @Test
+    @DisplayName("Reservation 생성 시 true 반환한다.")
+    void Reservation_생성_시_true_반환한다() {
+        var givenRoomId = "room-1";
+        var givenSeatId = "seat-1";
+        var givenUserId = "user-1";
+
+        var seatCreateCommand = new SeatCreateCommand(
+                givenRoomId,
+                givenSeatId
+        );
+
+        seatService.create(seatCreateCommand);
+
+        var startTime = Instant.parse("2025-06-01T01:00:00Z");
+        var endTime = Instant.parse("2025-06-01T02:00:00Z");
+
+        var reservationCreateCommand = new ReservationCreateCommand(
+                givenUserId,
+                givenRoomId,
+                givenSeatId,
+                startTime,
+                endTime
+        );
+
+        // Then
+        boolean result = reservationService.create(reservationCreateCommand);
+
+        Optional<Reservation> optStoredReservation = reservationStore.findByUserId(
+                givenUserId
+        );
+
+        assertThat(optStoredReservation.isPresent()).isTrue();
+
+        var storedReservation = optStoredReservation.get();
+
+        assertThat(result).isTrue();
+        assertThat(storedReservation.getUserId()).isEqualTo(givenUserId);
+        assertThat(storedReservation.getRoomId()).isEqualTo(givenRoomId);
+        assertThat(storedReservation.getSeatId()).isEqualTo(givenSeatId);
+    }
+}
