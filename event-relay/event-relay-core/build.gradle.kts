261aa29fde6a848f04986d89ef60e38c216486aa
lilamaris <lilamaris.dev@gmail.com>
2026-03-24 23:31:29 +0900

    feat(event-relay): 프로젝트 초기 설정

---
+++ event-relay/event-relay-core/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+}
+
+group = "com.seatliberator.seatliberator"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework:spring-context")
+    implementation("tools.jackson.core:jackson-databind")
+
+    implementation("org.slf4j:slf4j-api")
+
+    // Test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
