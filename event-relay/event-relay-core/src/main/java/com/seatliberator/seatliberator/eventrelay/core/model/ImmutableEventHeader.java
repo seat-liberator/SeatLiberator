4e67900a755b2c82f229e56678aeecd919e0652e
lilamaris <lilamaris.dev@gmail.com>
2026-03-25 04:05:11 +0900

    refactor(event-relay.model): 코어 모델 기본 Immutable 구현체 스펙 및 시그니처 정리
    - 하위 필드 또한 Immutable 구현체를 사용하도록 변경함
    - 클래스 명이 나타내는 불변성이 내부 필드까지 일관되게 적용되도록 정리함
    - 정적 팩토리 메서드 시그니처를 copyOf 로 통일하여 불변 객체로 복사함을 강조함

--- event-relay/event-relay-core/src/main/java/com/seatliberator/seatliberator/eventrelay/core/model/ImmutableEventHeader.java
+++ event-relay/event-relay-core/src/main/java/com/seatliberator/seatliberator/eventrelay/core/model/ImmutableEventHeader.java
@@ -3,9 +3,9 @@ package com.seatliberator.seatliberator.eventrelay.core.model;
 import org.jspecify.annotations.NonNull;
 
 public record ImmutableEventHeader(
-        @NonNull EventType eventType
+        @NonNull ImmutableEventType eventType
 ) implements EventHeader {
-    public static ImmutableEventHeader of(EventHeader eventHeader) {
+    public static ImmutableEventHeader copyOf(EventHeader eventHeader) {
         return new ImmutableEventHeader(ImmutableEventType.of(eventHeader.eventType()));
     }
 }
