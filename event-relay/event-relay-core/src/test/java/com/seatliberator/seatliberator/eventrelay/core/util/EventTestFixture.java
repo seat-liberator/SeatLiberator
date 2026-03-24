c2ab672d13562f334581fe5040ca75ade6a7f8fe
lilamaris <lilamaris.dev@gmail.com>
2026-03-25 05:21:17 +0900

    fix(event-relay.model): Test Fixture 수정
    - ImmutableEventEnvelope이 내부 필드도 Immutable 구현체로 변경되면서 테스트 유틸 클래스의 기존 생성 코드에서 타입이 안 맞는 문제 발생함
    - EventEnvelopeFactory를 주입받아서 생성하도록 변경

--- event-relay/event-relay-core/src/test/java/com/seatliberator/seatliberator/eventrelay/core/util/EventTestFixture.java
+++ event-relay/event-relay-core/src/test/java/com/seatliberator/seatliberator/eventrelay/core/util/EventTestFixture.java
@@ -1,6 +1,7 @@
 package com.seatliberator.seatliberator.eventrelay.core.util;
 
 import com.seatliberator.seatliberator.eventrelay.core.model.*;
+import com.seatliberator.seatliberator.eventrelay.core.model.factory.EventEnvelopeFactory;
 import tools.jackson.databind.ObjectMapper;
 
 import java.time.Clock;
@@ -53,16 +54,16 @@ public class EventTestFixture {
         return createTrace(createFixedClock());
     }
 
-    public static EventEnvelope createEnvelope(Clock clock) {
-        return new ImmutableEventEnvelope(
+    public static EventEnvelope createEnvelope(EventEnvelopeFactory factory, Clock clock) {
+        return factory.create(
                 createHeader(),
                 createTrace(clock),
                 createRawPayload()
         );
     }
 
-    public static EventEnvelope createEnvelope() {
-        return createEnvelope(createFixedClock());
+    public static EventEnvelope createEnvelope(EventEnvelopeFactory factory) {
+        return createEnvelope(factory, createFixedClock());
     }
 
     public static TestPayload createPayload() {
