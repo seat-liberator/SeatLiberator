f7b045a0eb8b46abaa8cd071d41b6efa44e00f72
lilamaris <lilamaris.dev@gmail.com>
2026-03-26 04:13:17 +0900

    feat(event-relay.provider): Provider 기본 구현체 추가
    - DefaultEventIdProvider: IdGenerator 기반 이벤트 ID 제공 구현체
    - DefaultCorrelationIdProvider: IdGenerator 기반 Correlation ID 제공 구현체
    - SpringApplicationNameProducerProvider: spring.application.name 속성 기반 Producer 제공 구현체

---
+++ event-relay/event-relay-core/src/main/java/com/seatliberator/seatliberator/eventrelay/core/provider/SpringApplicationNameProducerProvider.java
@@ -0,0 +1,20 @@
+package com.seatliberator.seatliberator.eventrelay.core.provider;
+
+import org.jspecify.annotations.NonNull;
+import org.springframework.core.env.Environment;
+
+public class SpringApplicationNameProducerProvider implements ProducerProvider {
+    private static final String LOCATION = "spring.application.name";
+    private static final String DEFAULT_UNKNOWN_ALIAS = "unknown-service";
+    private final Environment environment;
+
+    public SpringApplicationNameProducerProvider(@NonNull Environment environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public @NonNull String get() {
+        var name = environment.getProperty(LOCATION);
+        return (name == null || name.isBlank()) ? DEFAULT_UNKNOWN_ALIAS : name;
+    }
+}
