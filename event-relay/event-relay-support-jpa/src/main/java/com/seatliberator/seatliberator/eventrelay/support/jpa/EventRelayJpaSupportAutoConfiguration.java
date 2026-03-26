ade1a413a0f5f419a717fa17ece4fca844cc4382
lilamaris <lilamaris.dev@gmail.com>
2026-03-26 22:57:05 +0900

    feat(event-relay.store): AutoConfiguration 추가

---
+++ event-relay/event-relay-support-jpa/src/main/java/com/seatliberator/seatliberator/eventrelay/support/jpa/EventRelayJpaSupportAutoConfiguration.java
@@ -0,0 +1,37 @@
+package com.seatliberator.seatliberator.eventrelay.support.jpa;
+
+import com.seatliberator.seatliberator.eventrelay.core.store.EventStore;
+import com.seatliberator.seatliberator.eventrelay.core.store.EventStoreConfigurationProperties;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.data.jpa.autoconfigure.DataJpaRepositoriesAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@AutoConfiguration
+@AutoConfigureAfter(DataJpaRepositoriesAutoConfiguration.class)
+@ConditionalOnClass({EntityManager.class, JpaRepository.class})
+@EnableConfigurationProperties(EventStoreConfigurationProperties.class)
+public class EventRelayJpaSupportAutoConfiguration {
+
+    @Bean
+    EventStore eventStore(
+            JpaStoredEventRepository jpaStoredEventRepository,
+            EventAcceptor eventAcceptor,
+            EventStoreConfigurationProperties properties
+    ) {
+        return new JpaEventStore(jpaStoredEventRepository, eventAcceptor, properties.batchSize());
+    }
+
+    @Bean
+    @Qualifier("postgres")
+    @ConditionalOnMissingBean(EventAcceptor.class)
+    EventAcceptor eventAcceptor(EntityManager entityManager) {
+        return new JpaPostgresqlEventAcceptor(entityManager);
+    }
+}
