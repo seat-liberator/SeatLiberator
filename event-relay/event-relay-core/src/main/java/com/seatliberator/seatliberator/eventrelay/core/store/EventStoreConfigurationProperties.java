91342b2e73b6524343927e5493e5757fd1f5e839
lilamaris <lilamaris.dev@gmail.com>
2026-03-27 00:19:52 +0900

    fix(event-relay): AutoConfiguration 수정
    - EventStoreConfigurationProperties에서 batchSize의 기본 값을 지정함
    - EventListenerRegistry / EventListenerRegistrar 모두 Bean이 등록되지 않았을 경우에만 기본 구현체를 제공해서 한쪽만 bean override 되지 않도록 방어
    - EventDefinitionRegistry / EventDefinitionRegistrar도 마찬가지로 수정함

--- event-relay/event-relay-core/src/main/java/com/seatliberator/seatliberator/eventrelay/core/store/EventStoreConfigurationProperties.java
+++ event-relay/event-relay-core/src/main/java/com/seatliberator/seatliberator/eventrelay/core/store/EventStoreConfigurationProperties.java
@@ -1,9 +1,10 @@
 package com.seatliberator.seatliberator.eventrelay.core.store;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 
 @ConfigurationProperties(prefix = "event-relay.store")
 public record EventStoreConfigurationProperties(
-        int batchSize
+        @DefaultValue("100") int batchSize
 ) {
 }
