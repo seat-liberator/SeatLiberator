15934351ef12fa81975c9573c017666b3f7801ee
lilamaris <lilamaris.dev@gmail.com>
2026-03-28 07:04:57 +0900

    feat(identity): Spring Security 모듈에 AuthEntry 변경 사항 반영

--- identity/identity-application/src/main/java/com/seatliberator/seatliberator/identity/infrastructure/security/authentication/method/token/handler/TokenIssueProcessor.java
+++ identity/identity-application/src/main/java/com/seatliberator/seatliberator/identity/infrastructure/security/authentication/method/token/handler/TokenIssueProcessor.java
@@ -2,6 +2,8 @@ package com.seatliberator.seatliberator.identity.infrastructure.security.authent
 
 import com.seatliberator.seatliberator.identity.infrastructure.security.authentication.method.token.IssuedTokenEntry;
 
+import java.util.Set;
+
 public interface TokenIssueProcessor {
-    IssuedTokenEntry process(String subject);
+    IssuedTokenEntry process(String subject, Set<String> scopes);
 }
