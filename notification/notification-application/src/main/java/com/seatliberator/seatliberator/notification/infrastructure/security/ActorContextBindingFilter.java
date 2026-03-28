8abafc4562c37267d60397f92650e1a4a67dc844
lilamaris <lilamaris.dev@gmail.com>
2026-03-29 05:10:51 +0900

    feat(notification-app): Security Config 추가
    - ActorContextBindingFilter: JwtAuthenticationConverter을 통한 Actor 추출 과정 이후 요청 스레드 동안 Actor 정보를 Context Holder에 바인딩하기 위한 필터

---
+++ notification/notification-application/src/main/java/com/seatliberator/seatliberator/notification/infrastructure/security/ActorContextBindingFilter.java
@@ -0,0 +1,48 @@
+package com.seatliberator.seatliberator.notification.infrastructure.security;
+
+import com.seatliberator.seatliberator.identity.client.actor.ActorContextHolder;
+import com.seatliberator.seatliberator.identity.core.actor.Actor;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.NullMarked;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@NullMarked
+public class ActorContextBindingFilter extends OncePerRequestFilter {
+    private final ActorContextHolder actorContextHolder;
+
+    public ActorContextBindingFilter(ActorContextHolder actorContextHolder) {
+        this.actorContextHolder = actorContextHolder;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        var auth = SecurityContextHolder
+                .getContext().getAuthentication();
+
+        var principal = Optional.ofNullable(auth)
+                .map(Authentication::getPrincipal)
+                .filter(e -> e instanceof Actor)
+                .map(Actor.class::cast);
+
+        principal.ifPresent(actorContextHolder::setActor);
+
+        try {
+            filterChain.doFilter(request, response);
+        } finally {
+            actorContextHolder.clear();
+        }
+    }
+}
+
