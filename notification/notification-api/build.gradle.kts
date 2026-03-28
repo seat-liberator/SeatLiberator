9b434a0e8b09e9357538b5b0648e9acec12d6500
lilamaris <lilamaris.dev@gmail.com>
2026-03-29 04:31:03 +0900

    feat(notification-app): 프로젝트 초기 구성

---
+++ notification/notification-api/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+group = "com.seatliberator.seatliberator"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    api(project(":kernel"))
+    api(project(":event-relay:event-relay-core"))
+
+    implementation("org.springframework:spring-context")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ notification/notification-application/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    id("java")
+}
+
+group = "com.seatliberator.seatliberator"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // API
+    implementation(project(":notification:notification-api"))
+
+    // Web
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+
+    // Web Security
+    implementation(project(":identity:identity-client"))
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    // Persistence
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("com.h2database:h2")
+
+    // Event
+    implementation(project(":event-relay:event-relay-support-jpa"))
+    implementation(project(":event-relay:event-relay-support-kafka"))
+    implementation("org.springframework.boot:spring-boot-starter-kafka")
+
+    // Validation
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // Lombok
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    // Test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.testcontainers:testcontainers-junit-jupiter")
+    testImplementation("org.testcontainers:postgresql:1.21.3")
+    testImplementation("org.testcontainers:jdbc")
+    testRuntimeOnly("com.h2database:h2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
