8313ddd96ebb61e830847955c42ff3082767f418
lilamaris <lilamaris.dev@gmail.com>
2026-03-28 10:19:41 +0900

    fix(identity): Credential SignIn/SignUp Provider에서 하드코딩된 authorities 전달 수정

--- identity/identity-application/src/main/java/com/seatliberator/seatliberator/identity/infrastructure/security/authentication/method/credential/provider/CredentialSignUpProvider.java
+++ identity/identity-application/src/main/java/com/seatliberator/seatliberator/identity/infrastructure/security/authentication/method/credential/provider/CredentialSignUpProvider.java
@@ -17,7 +17,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -77,10 +77,15 @@ public class CredentialSignUpProvider implements AuthenticationProvider {
                 authEntry.userId()
         );
 
+        var authorities = trustedUserPrincipal.scopes().stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toUnmodifiableSet());
+
         var trustedAuthentication = new TrustedAuthenticationToken(
                 trustedUserPrincipal,
-                List.of(new SimpleGrantedAuthority("ROLE_USER"))
+                authorities
         );
+
         log.debug(
                 "Credential sign-up trusted authentication token created. email={}, userId={}",
                 email,
