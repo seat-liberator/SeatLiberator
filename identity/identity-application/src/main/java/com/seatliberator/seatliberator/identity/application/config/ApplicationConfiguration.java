4a1546b1e9e683334c7f5d42f9368b29b5be6f54
lilamaris <lilamaris.dev@gmail.com>
2026-03-29 14:43:31 +0900

    feat(identity.application): 사용자 등록 이벤트 발행을 연결함

--- identity/identity-application/src/main/java/com/seatliberator/seatliberator/identity/application/config/ApplicationConfiguration.java
+++ identity/identity-application/src/main/java/com/seatliberator/seatliberator/identity/application/config/ApplicationConfiguration.java
@@ -5,10 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.time.Clock;
+
 @Configuration
 public class ApplicationConfiguration {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    Clock clock() {
+        return Clock.systemUTC();
     }
 }
