e6ffdfef92de7a9217a710e4c3c3dbb16b870e74
lilamaris <lilamaris.dev@gmail.com>
2026-03-30 08:30:04 +0900

    test(reservation-app): 테스트 코드를 기능 패키지에 맞춰서 분리함

---
+++ reservation/reservation-application/src/test/java/com/seatliberator/seatliberator/reservation/TestFixture.java
@@ -0,0 +1,49 @@
+package com.seatliberator.seatliberator.reservation;
+
+import com.seatliberator.seatliberator.reservation.book.domain.Reservation;
+import com.seatliberator.seatliberator.reservation.vacancy.application.port.in.command.VacancyAlertRequestCommand;
+
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneOffset;
+
+import static org.assertj.core.api.Fail.fail;
+
+public class TestFixture {
+    public static final String INITIAL_USER_ID = "user-1";
+    public static final String INITIAL_ROOM_ID = "room-1";
+    public static final String INITIAL_SEAT_ID = "seat-1";
+    public static final Clock fixedClock = Clock.fixed(Instant.parse("2026-01-01T00:00:00Z"), ZoneOffset.UTC);
+    public static final Duration INITIAL_DURATION = Duration.ofMinutes(30);
+
+    public static Reservation createReservation(Instant startTime, Instant endTime) {
+        return Reservation.create(INITIAL_USER_ID, INITIAL_ROOM_ID, INITIAL_SEAT_ID, startTime, endTime);
+    }
+
+    public static Reservation createReservation() {
+        var startTime = fixedClock.instant();
+        var endTime = startTime.plus(INITIAL_DURATION);
+        return createReservation(startTime, endTime);
+    }
+
+    public static void stubReservationId(Reservation reservation, Long id) {
+        try {
+            var idField = Reservation.class.getDeclaredField("id");
+            idField.setAccessible(true);
+            idField.set(reservation, id);
+        } catch (ReflectiveOperationException e) {
+            fail("테스트용 ID 설정 실패");
+        }
+    }
+
+    public static VacancyAlertRequestCommand createVacancyAlertRequestCommand(Instant startTime, Instant endTime, Instant requestedAt) {
+        return new VacancyAlertRequestCommand(INITIAL_USER_ID, INITIAL_ROOM_ID, INITIAL_SEAT_ID, startTime, endTime, requestedAt);
+    }
+
+    public static VacancyAlertRequestCommand createVacancyAlertRequestCommand(Instant requestedAt) {
+        var startTime = fixedClock.instant();
+        var endTime = startTime.plus(INITIAL_DURATION);
+        return createVacancyAlertRequestCommand(startTime, endTime, requestedAt);
+    }
+}
--- reservation/reservation-application/src/test/java/com/seatliberator/seatliberator/reservation/unit/book/application/DefaultReservationReaderTest.java
+++ reservation/reservation-application/src/test/java/com/seatliberator/seatliberator/reservation/unit/book/application/DefaultReservationReaderTest.java
@@ -1,4 +1,4 @@
-package com.seatliberator.seatliberator.reservation.application;
+package com.seatliberator.seatliberator.reservation.unit.book.application;
 
 import com.seatliberator.seatliberator.reservation.book.application.exception.BookApplicationErrorCode;
 import com.seatliberator.seatliberator.reservation.book.application.exception.BookApplicationException;
@@ -6,27 +6,34 @@ import com.seatliberator.seatliberator.reservation.book.application.port.in.comm
 import com.seatliberator.seatliberator.reservation.book.application.port.in.command.SeatBasedReservationLocator;
 import com.seatliberator.seatliberator.reservation.book.application.port.out.ReservationStore;
 import com.seatliberator.seatliberator.reservation.book.application.service.DefaultReservationReader;
-import com.seatliberator.seatliberator.reservation.book.domain.Reservation;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.Instant;
 import java.util.Optional;
 
-import static com.seatliberator.seatliberator.reservation.ReservationTestFixture.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static com.seatliberator.seatliberator.reservation.TestFixture.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 @DisplayName("Application: Default Reservation Reader")
 public class DefaultReservationReaderTest {
-    private final ReservationStore reservationStore = mock(ReservationStore.class);
-    private final DefaultReservationReader reservationReader = new DefaultReservationReader(reservationStore);
+    @Mock
+    ReservationStore reservationStore;
+
+    @InjectMocks
+    DefaultReservationReader reservationReader;
 
     @Test
     @DisplayName("reservationId 기반 locator로 예약을 조회할 수 있다")
     void reservationId_기반_locator로_예약을_조회할_수_있다() {
-        Instant startTime = Instant.parse("2026-01-01T00:00:00Z");
-        var reservation = createReservation(startTime);
+        var reservation = createReservation();
 
         stubReservationId(reservation, 1L);
 
@@ -59,8 +66,7 @@ public class DefaultReservationReaderTest {
     @Test
     @DisplayName("seat 기반 locator로 예약을 조회할 수 있다")
     void seat_기반_locator로_예약을_조회할_수_있다() {
-        Instant startTime = Instant.parse("2026-01-01T00:00:00Z");
-        var reservation = createReservation(startTime);
+        var reservation = createReservation();
 
         stubReservationId(reservation, 1L);
 
@@ -127,15 +133,5 @@ public class DefaultReservationReaderTest {
         );
 
         assertEquals(BookApplicationErrorCode.RESERVATION_NOT_FOUND, exception.getErrorCode());
-    }
-
-    private void stubReservationId(Reservation reservation, Long reservationId) {
-        try {
-            var idField = Reservation.class.getDeclaredField("id");
-            idField.setAccessible(true);
-            idField.set(reservation, reservationId);
-        } catch (ReflectiveOperationException e) {
-            fail("테스트용 Reservation Id 설정에 실패했습니다.");
-        }
     }
 }
