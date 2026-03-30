29a35e4ad8625da6c29ad2c0cd7504f5a28215b0
lilamaris <lilamaris.dev@gmail.com>
2026-03-30 09:13:18 +0900

    refactor(board): 프로젝트 구조 변경
    - board-application 모듈에 애플리케이션 진입점을 두도록 변경함

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,11 @@
 rootProject.name = "seatliberator"
 
-include("board:board-api")
-include("board")
-
-include("reservation:reservation-api")
-include("reservation:reservation-application")
+include("kernel")
 
 include("identity:identity-core")
 include("identity:identity-client")
 include("identity:identity-api")
 include("identity:identity-application")
-
-include("kernel")
 
 include("idempotency:idempotency-core")
 
@@ -19,3 +13,9 @@ include("event-relay:event-relay-core")
 include("event-relay:event-relay-test")
 include("event-relay:event-relay-support-jpa")
 include("event-relay:event-relay-support-kafka")
+
+include("board:board-api")
+include("board:board-application")
+
+include("reservation:reservation-api")
+include("reservation:reservation-application")
