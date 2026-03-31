5a89f1a62cd0209aa4744ae7e994bde49205fcf7
lilamaris <lilamaris.dev@gmail.com>
2026-04-01 04:55:18 +0900

    feat(reservation-app): seat locator와 time range의 jpa predicates 추가

---
+++ reservation/reservation-application/src/main/java/com/seatliberator/seatliberator/reservation/shared/infrastructure/persistence/jpa/specification/TimeRangePredicates.java
@@ -0,0 +1,44 @@
+package com.seatliberator.seatliberator.reservation.shared.infrastructure.persistence.jpa.specification;
+
+import com.seatliberator.seatliberator.reservation.shared.domain.EmbeddableTimeRange;
+import com.seatliberator.seatliberator.reservation.shared.domain.TimeRange;
+import jakarta.persistence.criteria.From;
+import jakarta.persistence.criteria.Path;
+import org.springframework.data.jpa.domain.PredicateSpecification;
+
+import java.time.Instant;
+import java.util.function.Function;
+
+public class TimeRangePredicates {
+    public static <T> PredicateSpecification<T> withIn(
+            TimeRange range,
+            Function<From<?, T>, Path<EmbeddableTimeRange>> pathFunction
+    ) {
+        return (from, cb) -> {
+            var path = pathFunction.apply(from);
+
+            return cb.and(
+                    cb.lessThan(path.get("startAt"), range.endAt()),
+                    cb.greaterThan(path.get("endAt"), range.startAt())
+            );
+        };
+    }
+
+    public static <T> PredicateSpecification<T> contain(
+            Instant at,
+            Function<From<?, T>, Path<EmbeddableTimeRange>> pathFunction
+    ) {
+        return (from, cb) -> {
+            var path = pathFunction.apply(from);
+
+            return cb.and(
+                    cb.lessThan(path.get("startAt"), at),
+                    cb.greaterThan(path.get("endAt"), at)
+            );
+        };
+    }
+
+    public static <T> Function<From<?, T>, Path<EmbeddableTimeRange>> defaultRangePathFunction() {
+        return from -> from.get("range");
+    }
+}
