d3449f845abd3ef027c48c164a49ea76b232fa33
lilamaris <lilamaris.dev@gmail.com>
2026-04-01 06:02:28 +0900

    feat(reservation-app): 공통 Jpa predicate spec 유틸 추가
    - excludeIn: id 제외 조건 추가

---
+++ reservation/reservation-application/src/main/java/com/seatliberator/seatliberator/reservation/shared/infrastructure/persistence/jpa/specification/CommonPredicates.java
@@ -0,0 +1,27 @@
+package com.seatliberator.seatliberator.reservation.shared.infrastructure.persistence.jpa.specification;
+
+import jakarta.persistence.criteria.From;
+import jakarta.persistence.criteria.Path;
+import org.springframework.data.jpa.domain.PredicateSpecification;
+
+import java.util.Collection;
+import java.util.function.Function;
+
+public class CommonPredicates {
+    public static <T, V> PredicateSpecification<T> excludeIn(
+            Collection<V> values,
+            Function<From<?, T>, Path<V>> pathFunction
+    ) {
+        return (from, cb) -> {
+            if (values == null || values.isEmpty()) return cb.conjunction();
+            return cb.not(pathFunction.apply(from).in(values));
+        };
+    }
+
+    public static <T, V> PredicateSpecification<T> eq(V value, Function<From<?, T>, Path<V>> pathFunction) {
+        return (from, cb) -> {
+            var path = pathFunction.apply(from);
+            return value == null ? cb.isNull(path) : cb.equal(path, value);
+        };
+    }
+}
