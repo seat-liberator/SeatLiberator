d591369059d75179aa13ba205be8c785c12bd539
lilamaris <lilamaris.dev@gmail.com>
2026-04-21 20:16:23 +0900

    feat(reservation-app): room 도메인 초안
    - Seat 도메인에서 방 Id를 중복으로 저장하고 있음
    - 현재 Seat 도메인에서는 스터디룸 전역의 운영 정책을 표현하기 어려움 (운영/점검 시간, 휴무일 설정 등)
    - 기존 Seat 도메인의 스펙 변경은 최소화, Room 도메인 구체화 이후 변경 사항에 맞춰서 Seat 도메인 스펙을 정렬할 계획임
    - SeatLocator는 좌석 Id와 방 Id의 조합으로 표현되므로 Room이 애그리거트가 되서 자신의 RoomId와 하위에 속한 좌석의 SeatId를 조합하는 방향으로 점진적 확장 예정

---
+++ reservation/reservation-domain/src/main/java/com/seatliberator/seatliberator/reservation/domain/persistence/Room.java
@@ -0,0 +1,79 @@
+package com.seatliberator.seatliberator.reservation.domain.persistence;
+
+import com.seatliberator.seatliberator.reservation.domain.SeatLocator;
+import com.seatliberator.seatliberator.reservation.domain.SimpleSeatLocator;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "room")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "room_id", nullable = false, unique = true)
+    private String roomId;
+
+    @OneToMany(mappedBy = "room", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Seat> seats = new ArrayList<>();
+
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    private Room(
+            String roomId,
+            List<Seat> seats,
+            Instant createdAt
+    ) {
+        if (roomId == null || roomId.isBlank()) throw new IllegalArgumentException("roomId must not be null or blank.");
+        if (createdAt == null) throw new IllegalArgumentException("createdAt must not be null or blank.");
+
+        this.roomId = roomId;
+        this.seats = new ArrayList<>(seats);
+        this.createdAt = createdAt;
+
+        for (var seat : seats) {
+            ensureSeatBelongToRoom(seat);
+        }
+    }
+
+    public static Room of(String roomId, Instant createdAt) {
+        return new Room(roomId, List.of(), createdAt);
+    }
+
+    public static Room of(String roomId, List<Seat> seats, Instant createdAt) {
+        return new Room(roomId, seats, createdAt);
+    }
+
+    public void addSeat(Seat seat) {
+        ensureSeatBelongToRoom(seat);
+        this.seats.add(seat);
+    }
+
+    public SeatLocator locatorOf(Seat seat) {
+        ensureSeatBelongToRoom(seat);
+        return SimpleSeatLocator.of(roomId, seat.getLocator().seatId());
+    }
+
+    private void ensureSeatBelongToRoom(Seat seat) {
+        var locator = seat.getLocator();
+        if (!locator.roomId().equals(roomId)) {
+            throw new IllegalArgumentException(String.format(
+                    "seat must belong to room. seatId=%s, roomId expected=%s actual=%s",
+                    locator.seatId(),
+                    roomId,
+                    locator.roomId()
+            ));
+        }
+    }
+}
