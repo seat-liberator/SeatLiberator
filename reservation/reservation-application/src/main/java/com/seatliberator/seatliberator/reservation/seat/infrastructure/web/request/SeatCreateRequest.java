be718e632c6d566852b72ba0cdee4b870f0b5159
lilamaris <lilamaris.dev@gmail.com>
2026-04-21 16:21:57 +0900

    feat(reservation-app): seat api 관련 openapi 추가

--- reservation/reservation-application/src/main/java/com/seatliberator/seatliberator/reservation/seat/infrastructure/web/request/SeatCreateRequest.java
+++ reservation/reservation-application/src/main/java/com/seatliberator/seatliberator/reservation/seat/infrastructure/web/request/SeatCreateRequest.java
@@ -1,6 +1,10 @@
 package com.seatliberator.seatliberator.reservation.seat.infrastructure.web.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "좌석 생성 요청")
 public record SeatCreateRequest(
+        @Schema(description = "좌석 ID", example = "A-1")
         String seatId
 ) {
 }
--- reservation/reservation-application/src/main/java/com/seatliberator/seatliberator/reservation/seat/infrastructure/web/request/SeatUpdateRequest.java
+++ reservation/reservation-application/src/main/java/com/seatliberator/seatliberator/reservation/seat/infrastructure/web/request/SeatUpdateRequest.java
@@ -1,7 +1,12 @@
 package com.seatliberator.seatliberator.reservation.seat.infrastructure.web.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "좌석 정보 변경 요청")
 public record SeatUpdateRequest(
+        @Schema(description = "좌석이 속할 새로운 방 ID", example = "room-2")
         String newRoomId,
+        @Schema(description = "좌석의 새로운 ID", example = "B-1")
         String newSeatId
 ) {
 }
