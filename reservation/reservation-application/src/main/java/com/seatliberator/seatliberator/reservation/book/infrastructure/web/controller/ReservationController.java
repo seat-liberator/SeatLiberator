683f85afc0a863c13985dba8b6d464155bd0d63b
lilamaris <lilamaris.dev@gmail.com>
2026-04-21 15:30:29 +0900

    fix(reservation-app): 예약 수정 API 매핑 보정

--- reservation/reservation-application/src/main/java/com/seatliberator/seatliberator/reservation/book/infrastructure/web/controller/ReservationController.java
+++ reservation/reservation-application/src/main/java/com/seatliberator/seatliberator/reservation/book/infrastructure/web/controller/ReservationController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,7 @@ public class ReservationController {
 
     private final ActorContextHolder actorContextHolder;
 
+    @PutMapping
     public ResponseEntity<?> update(
             @RequestBody ReservationUpdateRequest request
     ) {
