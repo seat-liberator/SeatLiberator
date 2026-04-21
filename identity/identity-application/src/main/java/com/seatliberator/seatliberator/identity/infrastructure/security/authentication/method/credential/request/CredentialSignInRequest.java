5365882d70a0c3b9facaf05e5dfe0ac484d54dbf
lilamaris <lilamaris.dev@gmail.com>
2026-04-21 16:54:11 +0900

    feat(identity-app): 인증 api 관련 openapi 추가

--- identity/identity-application/src/main/java/com/seatliberator/seatliberator/identity/infrastructure/security/authentication/method/credential/request/CredentialSignInRequest.java
+++ identity/identity-application/src/main/java/com/seatliberator/seatliberator/identity/infrastructure/security/authentication/method/credential/request/CredentialSignInRequest.java
@@ -1,7 +1,12 @@
 package com.seatliberator.seatliberator.identity.infrastructure.security.authentication.method.credential.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "이메일 인증 로그인 요청")
 public record CredentialSignInRequest(
+        @Schema(description = "로그인 이메일", example = "user@example.com")
         String email,
+        @Schema(description = "로그인 비밀번호", example = "password1234!")
         String password
 ) {
 }
