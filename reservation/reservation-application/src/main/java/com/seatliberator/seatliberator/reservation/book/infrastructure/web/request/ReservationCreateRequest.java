2de893e1bb86a4c3b7a857569d96e498772b448e
lilamaris <lilamaris.dev@gmail.com>
2026-04-21 14:49:27 +0900

    fix(reservation-app): 예약 생성 API 분리
    - 자원 생성 시 방과 좌석이 필수인 점을 Endpoint 단계에서 명확하게 표현함

--- reservation/reservation-application/src/main/java/com/seatliberator/seatliberator/reservation/book/infrastructure/web/request/ReservationCreateRequest.java
+++ reservation/reservation-application/src/main/java/com/seatliberator/seatliberator/reservation/book/infrastructure/web/request/ReservationCreateRequest.java
@@ -3,8 +3,6 @@ package com.seatliberator.seatliberator.reservation.book.infrastructure.web.requ
 import java.time.Instant;
 
 public record ReservationCreateRequest(
-        String roomId,
-        String seatId,
         Instant startAt,
         Instant endAt
 ) {
