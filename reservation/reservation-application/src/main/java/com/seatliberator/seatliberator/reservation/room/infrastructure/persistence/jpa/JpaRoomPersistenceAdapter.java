9a9242a834e2683db1551f7b72c469a0da6d352e
lilamaris <lilamaris.dev@gmail.com>
2026-04-22 11:35:23 +0900

    feat(reservation-app): Room 도메인 용 Jpa Persistence Adapter 추가
    - 테스트
      - RoomReader의 조회 계약 준수 검증
      - RoomStore의 영속 계약 준수 검증

---
+++ reservation/reservation-application/src/main/java/com/seatliberator/seatliberator/reservation/room/infrastructure/persistence/jpa/JpaRoomPersistenceAdapter.java
@@ -0,0 +1,36 @@
+package com.seatliberator.seatliberator.reservation.room.infrastructure.persistence.jpa;
+
+import com.seatliberator.seatliberator.reservation.domain.persistence.Room;
+import com.seatliberator.seatliberator.reservation.room.application.port.out.RoomReader;
+import com.seatliberator.seatliberator.reservation.room.application.port.out.RoomStore;
+import com.seatliberator.seatliberator.reservation.room.infrastructure.persistence.jpa.repository.RoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class JpaRoomPersistenceAdapter implements RoomStore, RoomReader {
+    private final RoomRepository repository;
+
+    @Override
+    public boolean existsByRoomId(String roomId) {
+        return repository.existsByRoomId(roomId);
+    }
+
+    @Override
+    public Optional<Room> findByRoomId(String roomId) {
+        return repository.findByRoomId(roomId);
+    }
+
+    @Override
+    public Room save(Room room) {
+        return repository.save(room);
+    }
+
+    @Override
+    public void deleteByRoomId(String roomId) {
+        repository.deleteByRoomId(roomId);
+    }
+}
