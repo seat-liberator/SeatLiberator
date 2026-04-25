04e19cd738ea3e1d0d71662903ff763b0d9d7d12
lilamaris <lilamaris.dev@gmail.com>
2026-04-25 12:27:17 +0900

    feat(reservation-app): web mvc 실행 모듈 추가

---
+++ reservation/reservation-application/src/test/java/com/seatliberator/seatliberator/ReservationApplicationTestConfiguration.java
@@ -0,0 +1,7 @@
+package com.seatliberator.seatliberator;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationApplicationTestConfiguration {
+}
---
+++ reservation/reservation-web-mvc/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("java")
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+}
+
+group = "com.seatliberator.seatliberator"
+version = "0.0.1-SNAPSHOT"
+
+dependencies {
+    implementation(project(":reservation:reservation-application"))
+    implementation(project(":reservation:reservation-domain"))
+
+    // Web
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // Security
+    implementation(project(":identity:identity-client"))
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+
+    // Documentation
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
+
+    // Lombok
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    // Test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testImplementation("org.springframework.security:spring-security-test")
+
+    testImplementation(testFixtures(project(":reservation:reservation-domain")))
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
