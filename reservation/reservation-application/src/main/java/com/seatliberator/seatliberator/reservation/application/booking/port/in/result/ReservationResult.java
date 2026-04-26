6a391b5d228a75cc1918adb5496037a3fe406b4f
lilamaris <lilamaris.dev@gmail.com>
2026-04-26 21:07:21 +0900

    refactor(reservation): 예약 결과 객체 필드 확장

--- reservation/reservation-application/src/main/java/com/seatliberator/seatliberator/reservation/application/booking/port/in/result/ReservationResult.java
+++ reservation/reservation-application/src/main/java/com/seatliberator/seatliberator/reservation/application/booking/port/in/result/ReservationResult.java
@@ -3,20 +3,27 @@ package com.seatliberator.seatliberator.reservation.application.booking.port.in.
 import com.seatliberator.seatliberator.reservation.domain.ReservationStatus;
 import com.seatliberator.seatliberator.reservation.domain.persistence.Reservation;
 
+import java.time.Instant;
+
 public record ReservationResult(
         Long reservationId,
         String actorId,
         String roomId,
         String seatId,
+        Instant startAt,
+        Instant endAt,
         ReservationStatus status
 ) {
     public static ReservationResult of(Reservation reservation) {
         var locator = reservation.getLocator();
+        var range = reservation.getRange();
         return new ReservationResult(
                 reservation.getId(),
                 reservation.getUserId(),
                 locator.roomId(),
                 locator.seatId(),
+                range.startAt(),
+                range.endAt(),
                 reservation.getStatus()
         );
     }
