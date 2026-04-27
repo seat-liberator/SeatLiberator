e2d611613d2f79d25ea9c296011a26085f5c8837
lilamaris <lilamaris.dev@gmail.com>
2026-04-27 19:30:38 +0900

    fix(identity-app): Federated 로그인 흐름 정렬
    - Federated SignIn 흐름에서 신규 회원과 기존 회원 로그인 절차를 처리하는 Processor의 반환 타입을 AuthEntry로 변경해서 Actor 스펙 변경에 따른 영향을 줄임

--- identity/identity-application/src/main/java/com/seatliberator/seatliberator/identity/infrastructure/security/authentication/method/federated/handler/FederatedSignInProcessor.java
+++ identity/identity-application/src/main/java/com/seatliberator/seatliberator/identity/infrastructure/security/authentication/method/federated/handler/FederatedSignInProcessor.java
@@ -1,8 +1,8 @@
 package com.seatliberator.seatliberator.identity.infrastructure.security.authentication.method.federated.handler;
 
-import com.seatliberator.seatliberator.identity.core.actor.Actor;
+import com.seatliberator.seatliberator.identity.application.port.in.result.AuthEntry;
 import com.seatliberator.seatliberator.identity.infrastructure.security.authentication.method.federated.principal.FederatedPrincipal;
 
 public interface FederatedSignInProcessor {
-    Actor authenticate(FederatedPrincipal principal);
+    AuthEntry process(FederatedPrincipal principal);
 }
