694512701611553c0c39de5062edff81ef9e2df8
lilamaris <lilamaris.dev@gmail.com>
2026-04-28 00:10:29 +0900

    refactor(identity-core): NamespaceRoleCapabilitiesRegistry 위치 변경
    - NamespaceRole 컬렉션으로부터 Capability 얻는 메서드 추가

--- identity/identity-client-starter/src/main/java/com/seatliberator/seatliberator/identity/client/autoconfigure/IdentityClientAutoConfiguration.java
+++ identity/identity-client-starter/src/main/java/com/seatliberator/seatliberator/identity/client/autoconfigure/IdentityClientAutoConfiguration.java
@@ -2,7 +2,7 @@ package com.seatliberator.seatliberator.identity.client.autoconfigure;
 
 import com.seatliberator.seatliberator.identity.client.actor.ThreadLocalActorContextHolder;
 import com.seatliberator.seatliberator.identity.client.jwt.ActorContextJwtAuthenticationConverter;
-import com.seatliberator.seatliberator.identity.client.role.NamespaceRoleCapabilitiesRegistry;
+import com.seatliberator.seatliberator.identity.core.role.NamespaceRoleCapabilitiesRegistry;
 import com.seatliberator.seatliberator.identity.core.actor.ActorContextHolder;
 import com.seatliberator.seatliberator.identity.core.role.NamespaceRoleDeserializer;
 import com.seatliberator.seatliberator.identity.core.role.RoleCapabilities;
