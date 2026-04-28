77a1b0069b7e1a212545f8d760fd4962e235e175
lilamaris <lilamaris.dev@gmail.com>
2026-04-28 17:22:08 +0900

    feat(identity-core): Actor, Capability Test Fixture 추가

---
+++ identity/identity-core/src/testFixtures/java/com/seatliberator/seatliberator/identity/core/actor/ActorFixture.java
@@ -0,0 +1,49 @@
+package com.seatliberator.seatliberator.identity.core.actor;
+
+import com.seatliberator.seatliberator.identity.core.role.Capability;
+import com.seatliberator.seatliberator.identity.core.role.CapabilityFixture;
+
+import java.util.Set;
+
+public class ActorFixture {
+    public static final String INITIAL_SUBJECT = "user-subject-1";
+    public static final Set<Capability> INITIAL_CAPABILITIES = Set.of(
+            CapabilityFixture.createCapability("capability-1"),
+            CapabilityFixture.createCapability("capability-2"),
+            CapabilityFixture.createCapability("capability-3")
+    );
+
+    public static class Builder {
+        private String subject = INITIAL_SUBJECT;
+        private Set<Capability> capabilities = INITIAL_CAPABILITIES;
+
+        public Builder() {}
+
+        public Builder(String subject, Set<Capability> capabilities) {
+            this.subject = subject;
+            this.capabilities = capabilities;
+        }
+
+        public static Builder from(Builder other) {
+            return new Builder(other.subject, Set.copyOf(other.capabilities));
+        }
+
+        public Builder copy() {
+            return from(this);
+        }
+
+        public Builder subject(String subject) {
+            this.subject = subject;
+            return this;
+        }
+
+        public Builder capabilities(Set<Capability> capabilities) {
+            this.capabilities = Set.copyOf(capabilities);
+            return this;
+        }
+
+        public Actor build() {
+            return SimpleActor.of(subject, capabilities);
+        }
+    }
+}
---
+++ identity/identity-core/src/testFixtures/java/com/seatliberator/seatliberator/identity/core/role/CapabilityFixture.java
@@ -0,0 +1,52 @@
+package com.seatliberator.seatliberator.identity.core.role;
+
+public class CapabilityFixture {
+    public static final String INITIAL_SCOPE = "test-scope";
+    public static final String INITIAL_DESCRIPTION = "test scope description.";
+
+    public static Capability createCapability() {
+        return SimpleCapability.of(INITIAL_SCOPE, INITIAL_DESCRIPTION);
+    }
+
+    public static Capability createCapability(String scope) {
+        return SimpleCapability.of(scope, INITIAL_DESCRIPTION);
+    }
+
+    public static Capability createCapability(String scope, String description) {
+        return SimpleCapability.of(scope, description);
+    }
+
+    public static class Builder {
+        private String scope = INITIAL_SCOPE;
+        private String description = INITIAL_DESCRIPTION;
+
+        public Builder() {}
+
+        public Builder(String scope, String description) {
+            this.scope = scope;
+            this.description = description;
+        }
+
+        public static Builder from(Builder other) {
+            return new Builder(other.scope, other.description);
+        }
+
+        public Builder copy() {
+            return from(this);
+        }
+
+        public Builder scope(String scope) {
+            this.scope = scope;
+            return this;
+        }
+
+        public Builder description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Capability build() {
+            return SimpleCapability.of(scope, description);
+        }
+    }
+}
