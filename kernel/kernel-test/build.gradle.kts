b3574fe19d81d1607dc8ff979844751c00a6dc96
lilamaris <lilamaris.dev@gmail.com>
2026-05-03 19:48:44 +0900

    feat(kernel-test): 동시성 테스트 지원 모듈 추가

--- kernel/kernel-test/build.gradle.kts
+++ kernel/kernel-test/build.gradle.kts
@@ -6,6 +6,8 @@ group = "com.seatliberator.seatliberator"
 version = "0.0.1-SNAPSHOT"
 
 dependencies {
+    implementation(project(":kernel:kernel-core"))
+
     testImplementation("org.assertj:assertj-core")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
