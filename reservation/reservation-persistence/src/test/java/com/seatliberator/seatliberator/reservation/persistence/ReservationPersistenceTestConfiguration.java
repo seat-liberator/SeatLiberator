8f71431d213294e5f80aa275c95d46c8044bfad0
lilamaris <lilamaris.dev@gmail.com>
2026-05-06 03:47:04 +0900

    test(reservation-persistence): 기존 통합 테스트 정리

--- reservation/reservation-persistence/src/test/java/com/seatliberator/seatliberator/reservation/persistence/ReservationPersistenceTestConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.seatliberator.seatliberator.reservation.persistence;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
-
-@SpringBootApplication(scanBasePackages = "com.seatliberator.seatliberator.reservation")
-@EntityScan(basePackages = "com.seatliberator.seatliberator.reservation")
-public class ReservationPersistenceTestConfiguration {
-}
