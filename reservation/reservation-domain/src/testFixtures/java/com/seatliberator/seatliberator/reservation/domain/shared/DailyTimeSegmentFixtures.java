44cbe4cfe5b5b78cea7612fb8d70c92c505ccacb
lilamaris <lilamaris.dev@gmail.com>
2026-05-09 05:03:46 +0900

    feat(reservation-domain): Instant, ZoneId 기반 contains 비교 스펙 추가

--- reservation/reservation-domain/src/testFixtures/java/com/seatliberator/seatliberator/reservation/domain/shared/DailyTimeSegmentFixtures.java
+++ reservation/reservation-domain/src/testFixtures/java/com/seatliberator/seatliberator/reservation/domain/shared/DailyTimeSegmentFixtures.java
@@ -7,6 +7,19 @@ public class DailyTimeSegmentFixtures {
     public static final LocalTime START_AT = LocalTime.of(6, 0);
     public static final Duration DURATION = Duration.ofHours(8);
 
+    public static final DailyTimeSegment MORNING_SEGMENT = new DailyTimeSegmentFixtures.Builder()
+            .startAt(LocalTime.of(8, 0))
+            .duration(Duration.ofHours(4))
+            .build();
+    public static final DailyTimeSegment AFTERNOON_SEGMENT = new DailyTimeSegmentFixtures.Builder()
+            .startAt(LocalTime.of(13, 0))
+            .duration(Duration.ofHours(11))
+            .build();
+    public static final DailyTimeSegment DAWN_SEGMENT = new DailyTimeSegmentFixtures.Builder()
+            .startAt(LocalTime.of(1, 0))
+            .duration(Duration.ofHours(4))
+            .build();
+
     public static DailyTimeSegment get() {
         return SimpleDailyTimeSegment.of(START_AT, DURATION);
     }
