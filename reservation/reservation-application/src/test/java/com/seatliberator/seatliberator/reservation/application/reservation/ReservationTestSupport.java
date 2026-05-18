558cdd34674f18e44fe9894ea742abf820ea02da
lilamaris <lilamaris.dev@gmail.com>
2026-05-18 13:56:49 +0900

    feat(reservation-application): 예약 취소 권한 정책 객체 추가

--- reservation/reservation-application/src/test/java/com/seatliberator/seatliberator/reservation/application/reservation/ReservationTestSupport.java
+++ reservation/reservation-application/src/test/java/com/seatliberator/seatliberator/reservation/application/reservation/ReservationTestSupport.java
@@ -16,6 +16,10 @@ public class ReservationTestSupport extends DefaultTestSupport {
     public static final UUID RESERVATION_ID = UuidGenerator.generate(1);
     public static final String OTHER_USER_ID = "user-2";
     public static final Actor OTHER_ACTOR = new ActorFixture.Builder().subject(OTHER_USER_ID).build();
+    public static final Actor BOOKING_CANCELER = new ActorFixture.Builder()
+            .subject(USER_ID)
+            .capabilities(Set.of(ReservationCapability.OWNED_BOOKING_CANCEL))
+            .build();
     public static final Actor BOOKING_CREATOR = new ActorFixture.Builder()
             .subject(USER_ID)
             .capabilities(Set.of(ReservationCapability.BOOKING_CREATE))
