68a3b9f53a8cce7d3ba4e2ab46e041fd4f843dd7
lilamaris <lilamaris.dev@gmail.com>
2026-05-18 19:30:40 +0900

    fix(reservation-persistence): 사용하지 않는 Reservation Repository 쿼리 메서드 정리

--- reservation/reservation-persistence/src/main/java/com/seatliberator/seatliberator/reservation/persistence/book/jpa/repository/ReservationRepository.java
+++ reservation/reservation-persistence/src/main/java/com/seatliberator/seatliberator/reservation/persistence/book/jpa/repository/ReservationRepository.java
@@ -3,29 +3,10 @@ package com.seatliberator.seatliberator.reservation.persistence.book.jpa.reposit
 import com.seatliberator.seatliberator.reservation.domain.reservation.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.time.Instant;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface ReservationRepository extends JpaRepository<Reservation, UUID>, JpaSpecificationExecutor<Reservation> {
-
     List<Reservation> findByUserId(String userId);
-
-    @Query("""
-            SELECT r
-            FROM Reservation r
-            WHERE r.locator.roomId = :roomId
-                AND r.locator.seatId = :seatId
-                AND r.range.startAt <= :startAt
-                AND :endAt < r.range.endAt""")
-    Optional<Reservation> findReservationBySeatAt(
-            @Param("roomId") String roomId,
-            @Param("seatId") String seatId,
-            @Param("startAt") Instant startAt,
-            @Param("endAt") Instant endAt
-    );
 }
