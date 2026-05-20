27e53a68a7bdb0245782f82590e2e515b9f792e5
lilamaris <lilamaris.dev@gmail.com>
2026-05-21 02:17:58 +0900

    refactor(board): 도메인 모듈 분리

---
+++ board/board-domain/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("seatliberator.domain-base")
+}
+
+group = "com.seatliberator.seatliberator"
+version = "0.0.1-SNAPSHOT"
+
+dependencies {
+    implementation(project(":kernel:kernel-core"))
+    testImplementation(project(":kernel:kernel-test"))
+}
