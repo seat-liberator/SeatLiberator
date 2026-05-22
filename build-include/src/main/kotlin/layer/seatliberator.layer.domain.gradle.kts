f7529be12295d3a117c09151d267505852f7318f
lilamaris <lilamaris.dev@gmail.com>
2026-05-22 21:14:49 +0900

    fix(build): layer.domain convention plugin에 spring-data-commons 추가

--- build-include/src/main/kotlin/layer/seatliberator.layer.domain.gradle.kts
+++ build-include/src/main/kotlin/layer/seatliberator.layer.domain.gradle.kts
@@ -3,3 +3,11 @@ plugins {
     id("seatliberator.spring.jpa")
     id("java-test-fixtures")
 }
+
+val libs = extensions
+    .getByType<VersionCatalogsExtension>()
+    .named("libs")
+
+dependencies {
+    add("implementation", libs.findLibrary("spring-data-commons").get())
+}
