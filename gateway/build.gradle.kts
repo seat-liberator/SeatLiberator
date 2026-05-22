c3643567d4fa8e2574931cb73d7a55f095bda019
lilamaris <lilamaris.dev@gmail.com>
2026-05-22 20:44:53 +0900

    fix(gateway): 변경된 convention plugin 적용

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -1,20 +1,13 @@
 plugins {
-    id("seatliberator.spring-boot-application-base")
+    id("seatliberator.layer.launcher")
 }
 
 group = "com.seatliberator.seatliberator"
 version = "0.0.1-SNAPSHOT"
 
-val springCloudVersion = "2025.1.1"
-
-dependencyManagement {
-    imports {
-        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
-    }
-}
-
 dependencies {
-    implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webflux")
+    implementation(platform(libs.spring.cloud.dependencies))
+    implementation(libs.spring.cloud.starter.gateway.server.webflux)
 
     testImplementation(project(":identity:identity-application"))
     testImplementation(project(":reservation:reservation-web-mvc"))
