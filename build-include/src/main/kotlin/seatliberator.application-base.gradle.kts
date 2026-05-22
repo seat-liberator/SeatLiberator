a7139caa6368f930b68e59c82dc38049c659d9fd
lilamaris <lilamaris.dev@gmail.com>
2026-05-22 21:06:14 +0900

    chore(build): 사용하지 않는 convention plugin 정리

--- build-include/src/main/kotlin/seatliberator.application-base.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    id("seatliberator.module-base")
-    id("seatliberator.spring-module-base")
-}
-
-val libs = extensions
-    .getByType<VersionCatalogsExtension>()
-    .named("libs")
--- build-include/src/main/kotlin/seatliberator.spring-boot-monolith-base.gradle.kts
+++
@@ -1,5 +0,0 @@
-plugins {
-    id("seatliberator.spring-boot-application-base")
-    id("seatliberator.web-mvc-base")
-    id("seatliberator.persistence-base")
-}
