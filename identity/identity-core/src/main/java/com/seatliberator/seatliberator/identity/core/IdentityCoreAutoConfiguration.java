8adcce6ed16f0d4413cd56a2c65d2d718a3fe0ff
lilamaris <lilamaris.dev@gmail.com>
2026-05-24 08:33:00 +0900

    fix(identity-core): Introspection 스펙 제거

--- identity/identity-core/src/main/java/com/seatliberator/seatliberator/identity/core/IdentityCoreAutoConfiguration.java
+++ identity/identity-core/src/main/java/com/seatliberator/seatliberator/identity/core/IdentityCoreAutoConfiguration.java
@@ -1,7 +1,5 @@
 package com.seatliberator.seatliberator.identity.core;
 
-import com.seatliberator.seatliberator.identity.core.introspect.IntrospectionFactory;
-import com.seatliberator.seatliberator.identity.core.introspect.SimpleIntrospectionFactory;
 import com.seatliberator.seatliberator.identity.core.role.*;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -11,12 +9,6 @@ import org.springframework.context.annotation.Bean;
 @AutoConfiguration
 @EnableConfigurationProperties(IdentityRoleConfigurationProperties.class)
 public class IdentityCoreAutoConfiguration {
-    @Bean
-    @ConditionalOnMissingBean(IntrospectionFactory.class)
-    SimpleIntrospectionFactory simpleIntrospectionFactory() {
-        return new SimpleIntrospectionFactory();
-    }
-
     @Bean
     @ConditionalOnMissingBean(NamespaceRoleFormatter.class)
     SeparatorNamespaceRoleFormatter separatorNamespaceRoleFormatter(
