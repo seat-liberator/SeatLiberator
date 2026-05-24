955c8321085e80ac613d1fb3f809801c6f35275c
lilamaris <lilamaris.dev@gmail.com>
2026-05-25 06:46:28 +0900

    fix(application-starter): configuration properties null 체킹에 jakarta validation 사용

--- bootstrap/application-starter/src/main/java/com/seatliberator/seatliberator/bootstrap/application/ApplicationProperties.java
+++ bootstrap/application-starter/src/main/java/com/seatliberator/seatliberator/bootstrap/application/ApplicationProperties.java
@@ -1,19 +1,19 @@
 package com.seatliberator.seatliberator.bootstrap.application;
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.validation.annotation.Validated;
 
 import java.time.ZoneId;
 
+@Validated
 @ConfigurationProperties(prefix = "seatliberator.application")
 public record ApplicationProperties(
         @DefaultValue("true")
         boolean enabled,
 
         @DefaultValue("UTC")
-        ZoneId timezone
+        @NotNull ZoneId timezone
 ) {
-    public ApplicationProperties {
-        if (timezone == null) throw new IllegalArgumentException("timezone must not be null or blank.");
-    }
 }
