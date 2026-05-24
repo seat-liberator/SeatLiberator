52abfa50dc982c5f88c6f350d069a277712d83d2
lilamaris <lilamaris.dev@gmail.com>
2026-05-25 08:33:05 +0900

    feat(identity-application-starter): ActorContextHolder 자동 설정 추가

---
+++ identity/identity-application-starter/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("seatliberator.base.library")
+    id("seatliberator.spring.autoconfigure")
+}
+
+group = "com.seatliberator.seatliberator"
+version = "0.0.1-SNAPSHOT"
+
+dependencies {
+    api(project(":identity:identity-core"))
+    api(project(":bootstrap:application-starter"))
+}
