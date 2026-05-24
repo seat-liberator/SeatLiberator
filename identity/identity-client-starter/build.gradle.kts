899b549652c2bf7f87d7748dbe4b3a7a8a835043
lilamaris <lilamaris.dev@gmail.com>
2026-05-24 21:05:33 +0900

    fix(identity-client-starter): JWT 자동 설정 구성을 정리

--- identity/identity-client-starter/build.gradle.kts
+++ identity/identity-client-starter/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    id("java")
+    id("seatliberator.base.library")
+    id("seatliberator.spring.security")
+    id("seatliberator.spring.autoconfigure")
 }
 
 group = "com.seatliberator.seatliberator"
@@ -8,28 +10,5 @@ version = "0.0.1-SNAPSHOT"
 dependencies {
     api(project(":identity:identity-core"))
 
-    // Validation
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-
-    // Jwt
-    implementation("org.springframework.security:spring-security-oauth2-jose")
-
-    // Web
-    implementation("org.springframework:spring-web")
-    implementation("tools.jackson.core:jackson-databind")
-    compileOnly("jakarta.servlet:jakarta.servlet-api")
-
-    // Logging
-    implementation("org.slf4j:slf4j-api")
-
-    // Test
-    testImplementation(project(":identity:identity-core"))
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-tasks.test {
-    useJUnitPlatform()
+    implementation(libs.spring.security.jose)
 }
