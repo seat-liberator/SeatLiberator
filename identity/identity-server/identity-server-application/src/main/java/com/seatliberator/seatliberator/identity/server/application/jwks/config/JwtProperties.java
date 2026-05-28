3c8df47bdce995e6c9949d1e20940dfcae5f1407
lilamaris <lilamaris.dev@gmail.com>
2026-05-28 23:44:02 +0900

    fix(identity-server-application): Jwt 설정 필드 및 Validation 추가

--- identity/identity-server/identity-server-application/src/main/java/com/seatliberator/seatliberator/identity/server/application/jwks/config/JwtProperties.java
+++ identity/identity-server/identity-server-application/src/main/java/com/seatliberator/seatliberator/identity/server/application/jwks/config/JwtProperties.java
@@ -1,14 +1,24 @@
 package com.seatliberator.seatliberator.identity.server.application.jwks.config;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.validation.annotation.Validated;
 
 import java.time.Duration;
+import java.util.List;
 
+@Validated
 @ConfigurationProperties(prefix = "identity.jwt")
 public record JwtProperties(
-        Duration expiration
+        @DefaultValue("seatliberator-identity")
+        @NotBlank String issuer,
+
+        @DefaultValue({"seatliberator-api"})
+        @NotNull List<@NotBlank String> audience,
+
+        @DefaultValue("PT15M")
+        @NotNull Duration expiration
 ) {
-    public JwtProperties {
-        expiration = expiration != null ? expiration : Duration.ofMillis(900000);
-    }
 }
