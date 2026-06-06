ee077390aecaaa490e5098c3a9e1e30eebf94b51
lilamaris <lilamaris.dev@gmail.com>
2026-06-07 07:29:11 +0900

    fix(reservation-web-mvc): 테스트용 부트 설정 추가

---
+++ reservation/reservation-web-mvc/src/test/java/com/seatliberator/seatliberator/reservation/ReservationWebMvcTestApplication.java
@@ -0,0 +1,7 @@
+package com.seatliberator.seatliberator.reservation;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationWebMvcTestApplication {
+}
